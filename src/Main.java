a796ad37208cf4f9de6311fb66cd2fdd5cb39e0b
AntND <antonlenz508@gmail.com>
2025-09-03 19:05:39 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] ints = new int[11];
+
+        for (int i = 0; i < ints.length ; i++){
+            ints[i] = 10-i;
+        }
+
+        binarySearch(ints);
+
+    }
+
+
+    public static void binarySearch(int[] ints){
+
+        Arrays.sort(ints);
+
+        int M;
+        int L = 0;
+        int R = ints.length-1;
+        int num = 6;
+        boolean bol = true;
+
+        System.out.println(Arrays.toString(ints));
+
+        while (bol){
+
+            M = (R+L)/2;
+
+            if (num == ints[M]){
+                M++;
+                System.out.println("num is at: " + M);
+                bol = false;
+            }
+            else if (num > ints[M]){
+                L = M;
+            }
+            else if (num < ints[M]){
+                R = M;
+            }
         }
     }
+
 }
